dec23402ffd263c41661c1693bd5b3b3a454b853
Vilson <instrutorvilsonmoro@gmail.com>
2022-06-06 19:27:09 -0300

    Criar manipulador de exceçaõ

---
+++ src/main/java/com/aulas/rest/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.aulas.rest.exceptions;
+
+import java.time.Instant;
+
+import javax.persistence.EntityNotFoundException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {	
+	
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest req){
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(HttpStatus.NOT_FOUND.value());
+		err.setError("Recurso não encontrado");
+		err.setMessage(e.getMessage());
+		err.setPath(req.getRequestURI());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+}
